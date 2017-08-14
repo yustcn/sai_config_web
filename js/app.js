684264ad5d39f4b148267b0db26253a41db008f3
蔡劲松 <caijinsong@soundai.com>
2017-08-14 14:21:42 +0800

    use a one tomcat webapps instead of two

--- js/app.js
+++ js/app.js
@@ -4,31 +4,32 @@ angular.module('RawdataDownloadTool', [])
     $http.defaults.headers.post['dataType'] = 'json'
     $scope.isShow=false;
 
-    $scope.submit = function() {
-        var url = "http://cs-" + $scope.stage + ".sai.corp:8000/rawdata/v1/search"
-        $http.post(url, {'clientId': $scope.clientId, 'audioType': $scope.audioType,
-                            'beginTime':$scope.beginTime,
-                            'endTime': $scope.endTime }
-            )
-            .then(function successCallback(response) {
-                if(response.status == 200 && response.data.errorCode == 0){
-                    $scope.resultItems = response.data.rawDataEntities;
-                    angular.forEach($scope.resultItems, function(item){
-                        item.createTime = new Date(item.createTime).toISOString();
-                    });
-                    if ($scope.resultItems && $scope.resultItems.length > 0) {
-                        $scope.downloadUrl=response.data.downLoadUrl;
-                        $scope.isShow=true;
+    $scope.submit = function(isValid) {
+        if (isValid) {
+            $http.post("/rawdata/v1/search", {'clientId': $scope.clientId, 'audioType': $scope.audioType,
+                                'beginTime':$scope.beginTime,
+                                'endTime': $scope.endTime }
+                )
+                .then(function successCallback(response) {
+                    if(response.status == 200 && response.data.errorCode == 0){
+                        $scope.resultItems = response.data.rawDataEntities;
+                        angular.forEach($scope.resultItems, function(item){
+                            item.createTime = new Date(item.createTime).toISOString();
+                        });
+                        if ($scope.resultItems && $scope.resultItems.length > 0) {
+                            $scope.downloadUrl=response.data.downLoadUrl;
+                            $scope.isShow=true;
+                        } else {
+                            alert("没有找到结果.")
+                            $scope.isShow=false;
+                        }
                     } else {
-                        alert("没有找到结果.")
-                        $scope.isShow=false;
+                        alert("error " + response.data.errorCode + " errorMsg " + response.data.errorMessage)
                     }
-                } else {
-                    alert("error " + response.data.errorCode + " errorMsg " + response.data.errorMessage)
-                }
-            }, function errorCallback(response) {
-                alert("error " + response.status);
-            });
+                }, function errorCallback(response) {
+                    alert("error " + response.status);
+                });
+        }
 
     };
 });
